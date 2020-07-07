aa10aa3de3097eabc281a6159106d2baebecbd81
Tobias Antensteiner <tobias.antensteiner@schule.at>
2020-07-07 02:43:06 +0200

    Add core functionality
    Add core functionality to display mathematical notations using the KaTeX math typesetting library.

--- Automath.php
+++ Automath.php
@@ -26,10 +26,66 @@ class Automath {
 
 	public function Automath($options, $Automad) {
 
-		$htmlOutput = '<strong>Antstei's automath extension is (almost) working :D</strong>';
+		// Define some defaults.
+		$defaults = array(
+			'source' => 'cdn',
+			'defer' => False,
+			'element' => 'document.body',
+			'delimiters' => [['left' => '$$', 'right' => '$$', 'display' => True]],
+			'ignoredTags' => ['script', 'noscript', 'style', 'textarea', 'pre', 'code'],
+			'ignoredClasses' => [],
+			'errorCallback' => 'console.error',
+		);
+
+		// Merge defaults with predefined options.
+		if (array_key_exists('delimiters', $options)) {
+			$options['delimiters'] = json_decode($options['delimiters'], True);
+			$options['delimiters'] = array_merge($defaults['delimiters'], $options['delimiters']);
+		}
+
+		if (array_key_exists('ignoredTags', $options)) {
+			$options['ignoredTags'] = json_decode($options['ignoredTags'], True);
+			$options['ignoredTags'] = array_merge($defaults['ignoredTags'], $options['ignoredTags']);
+		}
+
+		if (array_key_exists('ignoredClasses', $options)) {
+			$options['ignoredClasses'] = json_decode($options['ignoredClasses'], True);
+			$options['ignoredClasses'] = array_merge($defaults['ignoredClasses'], $options['ignoredClasses']);
+		}
+
+		$options = array_merge($defaults, $options);
+
+		//
+		$htmlOutput = '<!-- -->';
+
+		// Source
+		switch ($options['source']) {
+			case 'cdn':
+			default:
+				$htmlOutput .= '<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.11.1/dist/katex.min.css" integrity="sha384-zB1R0rpPzHqg7Kpt0Aljp8JPLqbXI3bhnPWROx27a9N0Ll6ZP/+DiW/UqRcLbRjq" crossorigin="anonymous">';
+				$htmlOutput .= '<script ' . ($options['defer'] ? 'defer ' : '') . 'src="https://cdn.jsdelivr.net/npm/katex@0.11.1/dist/katex.min.js" integrity="sha384-y23I5Q6l+B6vatafAwxRu/0oK/79VlbSz7Q9aiSZUvyWYIYsd+qj+o24G5ZU2zJz" crossorigin="anonymous"></script>';
+				$htmlOutput .= '<script ' . ($options['defer'] ? 'defer ' : '') . 'src="https://cdn.jsdelivr.net/npm/katex@0.11.1/dist/contrib/auto-render.min.js" integrity="sha384-kWPLUVMOks5AQFrykwIup5lo0m3iMkkHrD0uJ4H5cjeGihAutqP0yW0J6dpFiVkI" crossorigin="anonymous"></script>';
+				break;
+		}
+
+		// Element
+		$htmlOutput .= '<script>document.addEventListener("DOMContentLoaded",function(){renderMathInElement(' . $options['element'] . ',';
+
+		// Delimiters
+		$htmlOutput .= '{delimiters:' . json_encode($options['delimiters']) . ',';
+
+		// Ignored tages
+		$htmlOutput .= 'ignoredTags:' . json_encode($options['ignoredTags']) . ',';
+
+		// Ignored classes
+		$htmlOutput .= 'ignoredClasses:' . json_encode($options['ignoredClasses']) . ',';
+
+		// Error callback
+		$htmlOutput .= 'errorCallback:' . $options['errorCallback'];
+
+		$htmlOutput .= '});});</script>';
 
 		return $htmlOutput;
+		
 	}
-
-
 }
--- README.md
+++ README.md
@@ -1,3 +1,25 @@
 # Automad Automath Extension
 
 This extension enables you to display beautiful mathematical notations within your pages by using the KaTeX math typesetting library.
+
+## Usage
+
+This extension can be enabled on pages by calling
+
+	<@ Antstei/Automath { options } @>
+
+inside the head section (`<head> ... </head>`) of the respective template.
+
+## Options
+
+The following options are available:
+
+| Name |
+|------|
+| `source` |
+| `defer` |
+| `element` |
+| `delimiters` |
+| `ignoredTags` |
+| `ignoredClasses` |
+| `errorCallback` |
