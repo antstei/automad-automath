fddc1bebff1804b40ab7ec7c5c7ab1784ae21bfc
Tobias Antensteiner <tobias.antensteiner@schule.at>
2020-07-10 22:31:46 +0200

    Add version 1.1.0
    Add version 1.1.0 to
    • add `includedPageTags` and `includedQueryFields` options,
    • add description of Automath's internal options to the documentation README.md

--- Automath.php
+++ Automath.php
@@ -2,7 +2,7 @@
 /**
  *	Automad Automath
  *
- * 	An automad extension to display beautiful mathematical notations using the KaTeX math typesetting library.
+ * 	An Automad extension to display beautiful mathematical notations using the KaTeX math typesetting library.
  *
  * 	@author Tobias Antensteiner
  * 	@copyright Copyright (C) 2020 Tobias Antensteiner - <https://tobis.website>
@@ -16,148 +16,184 @@ defined('AUTOMAD') or die('Direct access not permitted!');
 
 class Automath {
 
-	/**
-	 * 	Main function to build KaTeX bootstrap code.
-	 *
-	 * 	@param array $options
-	 * 	@param object $Automad
-	 * 	@return string KaTeX bootstrap code
-	 */
+    /**
+    * 	Main function to build KaTeX bootstrap code.
+    *
+    * 	@param array $options
+    * 	@param object $Automad
+    * 	@return string KaTeX bootstrap code
+    */
 
-	public function Automath($options, $Automad) {
+    public function Automath($options, $Automad) {
+        /* Define defaults. */
+        $defaults = array(
+            // Automath options.
+            'source' => 'cdn',
+            'defer' => false,
+            'includedPageTags' => array(),
+            'includedQueryFields' => array('filter', 'search'),
 
-		/* Define defaults. */
-		$defaults = array(
-			// Automath options
-			'source' => 'cdn',
-			'defer' => false,
+            // Supported KaTeX API options.
+            'element' => 'document.body',
+            'delimiters' => array(array('left' => '$', 'right' => '$', 'display' => false)),
+            'ignoredTags' => array(),
+            'ignoredClasses' => array(),
+            'errorCallback' => null,
+            'preProcess' => null
+        );
 
-			// Supported KaTeX API options
-			'element' => 'document.body',
-			'delimiters' => array(array('left' => '$', 'right' => '$', 'display' => false)),
-			'ignoredTags' => array(),
-			'ignoredClasses' => array(),
-			'errorCallback' => null,
-			'preProcess' => null
-		);
+        /* Create start of KaTeX bootstrap code string. */
+        $htmlOutput = '';
 
-		/* Merge predefined defaults with validated provided options. */
+        /* Merge predefined defaults of list of page tags to include and of list of query fields to include with validated provided options. */
+        // Check if provided list of page tags to include is a valid list.
+        if (array_key_exists('includedPageTags', $options)) {
+            $includedPageTags = json_decode($options['includedPageTags'], true);
+            if (is_array($includedPageTags) and json_last_error() === JSON_ERROR_NONE) {
+                $options['includedPageTags'] = array_unique(array_merge($defaults['includedPageTags'], $includedPageTags), SORT_REGULAR);
+            } else {
+                Automad\Debug::log($options['includedPageTags'], ucfirst(self::getJSONErrorDescription()) . ' occurred while parsing provided list of page tags to include');
+                $options['includedPageTags'] = $defaults['includedPageTags'];
+            }
+        } else {
+            $options['includedPageTags'] = $defaults['includedPageTags'];
+        }
 
-		// Check if provided list of delimiters is a valid list of KaTeX delimiters.
-		if (array_key_exists('delimiters', $options)) {
-			$delimiters = json_decode($options['delimiters'], true);
-			if (is_array($delimiters) and
-					!empty($delimiters) and
-					array_keys($delimiters) === range(0, count($delimiters)-1) and
-					count($delimiters) < 2 ? array_keys($defaults['delimiters'][0]) === array_keys($delimiters[0]) :
-						array_keys($defaults['delimiters'][0]) === array_keys(call_user_func_array('array_intersect_key', $delimiters)) and
-					array_keys($defaults['delimiters'][0]) === array_keys(call_user_func_array('array_merge', $delimiters)) and
-					json_last_error() === JSON_ERROR_NONE) {
-				$options['delimiters'] = $delimiters;
-				$options['delimiters'] = array_unique(array_merge($defaults['delimiters'], $options['delimiters']), SORT_REGULAR);
-			} else {
-				Automad\Debug::log($options['delimiters'], ucfirst(self::getJSONErrorDescription()) . ' occurred while parsing provided list of delimiters');
-				$options['delimiters'] = $defaults['delimiters'];
-			}
-		}
+        // Check if provided list of query strings to include is a valid list.
+        if (array_key_exists('includedQueryFields', $options)) {
+            $includedQueryFields = json_decode($options['includedQueryFields'], true);
+            if (is_array($includedQueryFields) and json_last_error() === JSON_ERROR_NONE) {
+                $options['includedQueryFields'] = array_unique(array_merge($defaults['includedQueryFields'], $includedQueryFields), SORT_REGULAR);
+            } else {
+                Automad\Debug::log($options['includedQueryFields'], ucfirst(self::getJSONErrorDescription()) . ' occurred while parsing provided list of query fields to include');
+                $options['includedQueryFields'] = $defaults['includedQueryFields'];
+            }
+        } else {
+            $options['includedQueryFields'] = $defaults['includedQueryFields'];
+        }
 
-		// Check if provided list of tags to ignore is a valid KaTeX list of DOM node types to ignore.
-		if (array_key_exists('ignoredTags', $options)) {
-			$ignoredTags = json_decode($options['ignoredTags'], true);
-			if (is_array($ignoredTags) and !empty($ignoredTags) and json_last_error() === JSON_ERROR_NONE) {
-				$options['ignoredTags'] = $ignoredTags;
-				$options['ignoredTags'] = array_merge($defaults['ignoredTags'], $options['ignoredTags']);
-			} else {
-				Automad\Debug::log($options['ignoredTags'], ucfirst(self::getJSONErrorDescription()) . ' occurred while parsing provided list of DOM node types to ignore');
-				$options['ignoredTags'] = $defaults['ignoredTags'];
-			}
-		}
+        /* Check if current page's list of page tags is subset of list of page tags to include or current page's list of query fields is subset of list of query fields to include. */
+        if (!empty($options['includedPageTags']) and array_intersect($Automad->Context->get()->tags, $options['includedPageTags']) === array() and
+                !empty($options['includedQueryFields']) and array_intersect(array_keys($_GET), $options['includedQueryFields']) === array()) {
+            $htmlOutput .= '';
+        } else {
+            /* Merge remaining not scalar-typed predefined defaults with validated provided options. */
 
-		// Check if provided list of classes to ignore is a valid KaTeX list of DOM node class names to ignore.
-		if (array_key_exists('ignoredClasses', $options)) {
-			$ignoredClasses = json_decode($options['ignoredClasses'], true);
-			if (is_array($ignoredClasses) and !empty($ignoredClasses) and json_last_error() === JSON_ERROR_NONE) {
-				$options['ignoredClasses'] = $ignoredClasses;
-				$options['ignoredClasses'] = array_merge($defaults['ignoredClasses'], $options['ignoredClasses']);
-			} else {
-				Automad\Debug::log($options['ignoredClasses'], ucfirst(self::getJSONErrorDescription()) . ' occurred while parsing provided list of DOM node class names to ignore');
-				$options['ignoredClasses'] = $defaults['ignoredClasses'];
-			}
-		}
+            // Check if provided list of delimiters is a valid list of KaTeX delimiters.
+            if (array_key_exists('delimiters', $options)) {
+                $delimiters = json_decode($options['delimiters'], true);
+                if (is_array($delimiters) and
+                        !empty($delimiters) and
+                        array_keys($delimiters) === range(0, count($delimiters)-1) and
+                        count($delimiters) < 2 ? array_keys($defaults['delimiters'][0]) === array_keys($delimiters[0]) :
+                        array_keys($defaults['delimiters'][0]) === array_keys(call_user_func_array('array_intersect_key', $delimiters)) and
+                        array_keys($defaults['delimiters'][0]) === array_keys(call_user_func_array('array_merge', $delimiters)) and
+                        json_last_error() === JSON_ERROR_NONE) {
+                    $options['delimiters'] = array_unique(array_merge($defaults['delimiters'], $delimiters), SORT_REGULAR);
+                } else {
+                    Automad\Debug::log($options['delimiters'], ucfirst(self::getJSONErrorDescription()) . ' occurred while parsing provided list of delimiters');
+                    $options['delimiters'] = $defaults['delimiters'];
+                }
+            }
 
-		// Merge remaining predefined defaults with provided options.
-		$options = array_merge($defaults, $options);
+            // Check if provided list of tags to ignore is a valid KaTeX list of DOM node types to ignore.
+            if (array_key_exists('ignoredTags', $options)) {
+                $ignoredTags = json_decode($options['ignoredTags'], true);
+                if (is_array($ignoredTags) and !empty($ignoredTags) and json_last_error() === JSON_ERROR_NONE) {
+                    $options['ignoredTags'] = array_unique(array_merge($defaults['ignoredTags'], $ignoredTags), SORT_REGULAR);
+                } else {
+                    Automad\Debug::log($options['ignoredTags'], ucfirst(self::getJSONErrorDescription()) . ' occurred while parsing provided list of DOM node types to ignore');
+                    $options['ignoredTags'] = $defaults['ignoredTags'];
+                }
+            }
 
-		/* Build HTML output of KaTeX bootstrap code. */
-		$htmlOutput = '<!-- antstei/automath-chunk start: autogenerated HTML code to display beautiful mathematical notations using the KaTeX math typesetting library. -->';
+            // Check if provided list of classes to ignore is a valid KaTeX list of DOM node class names to ignore.
+            if (array_key_exists('ignoredClasses', $options)) {
+                $ignoredClasses = json_decode($options['ignoredClasses'], true);
+                if (is_array($ignoredClasses) and !empty($ignoredClasses) and json_last_error() === JSON_ERROR_NONE) {
+                    $options['ignoredClasses'] = array_unique(array_merge($defaults['ignoredClasses'], $ignoredClasses), SORT_REGULAR);
+                } else {
+                    Automad\Debug::log($options['ignoredClasses'], ucfirst(self::getJSONErrorDescription()) . ' occurred while parsing provided list of DOM node class names to ignore');
+                    $options['ignoredClasses'] = $defaults['ignoredClasses'];
+                }
+            }
 
-		// Include KaTeX source.
-		switch ($options['source']) {
-			case 'cdn':
-			default:
-				$htmlOutput .= '<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.11.1/dist/katex.min.css" integrity="sha384-zB1R0rpPzHqg7Kpt0Aljp8JPLqbXI3bhnPWROx27a9N0Ll6ZP/+DiW/UqRcLbRjq" crossorigin="anonymous">';
-				$htmlOutput .= '<script ' . ($options['defer'] ? 'defer ' : '') . 'src="https://cdn.jsdelivr.net/npm/katex@0.11.1/dist/katex.min.js" integrity="sha384-y23I5Q6l+B6vatafAwxRu/0oK/79VlbSz7Q9aiSZUvyWYIYsd+qj+o24G5ZU2zJz" crossorigin="anonymous"></script>';
-				$htmlOutput .= '<script ' . ($options['defer'] ? 'defer ' : '') . 'src="https://cdn.jsdelivr.net/npm/katex@0.11.1/dist/contrib/auto-render.min.js" integrity="sha384-kWPLUVMOks5AQFrykwIup5lo0m3iMkkHrD0uJ4H5cjeGihAutqP0yW0J6dpFiVkI" crossorigin="anonymous"></script>';
-				break;
-		}
+            // Merge remaining scalar-typed predefined defaults with provided options.
+            $options = array_merge($defaults, $options);
 
-		// Supply HTML DOM element.
-		$htmlOutput .= '<script>document.addEventListener("DOMContentLoaded",function(){renderMathInElement(' . $options['element'] . ',';
+            /* Build HTML output of KaTeX bootstrap code. */
+            $htmlOutput .= '<!-- antstei/automath-chunk start: autogenerated HTML code to display beautiful mathematical notations using the KaTeX math typesetting library. -->';
 
-		// Supply delimiters.
-		$htmlOutput .= '{delimiters:' . json_encode($options['delimiters']) . ',';
+            // Include KaTeX source.
+            switch ($options['source']) {
+                case 'cdn':
+                default:
+                    $htmlOutput .= '<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.11.1/dist/katex.min.css" integrity="sha384-zB1R0rpPzHqg7Kpt0Aljp8JPLqbXI3bhnPWROx27a9N0Ll6ZP/+DiW/UqRcLbRjq" crossorigin="anonymous">';
+                    $htmlOutput .= '<script ' . ($options['defer'] ? 'defer ' : '') . 'src="https://cdn.jsdelivr.net/npm/katex@0.11.1/dist/katex.min.js" integrity="sha384-y23I5Q6l+B6vatafAwxRu/0oK/79VlbSz7Q9aiSZUvyWYIYsd+qj+o24G5ZU2zJz" crossorigin="anonymous"></script>';
+                    $htmlOutput .= '<script ' . ($options['defer'] ? 'defer ' : '') . 'src="https://cdn.jsdelivr.net/npm/katex@0.11.1/dist/contrib/auto-render.min.js" integrity="sha384-kWPLUVMOks5AQFrykwIup5lo0m3iMkkHrD0uJ4H5cjeGihAutqP0yW0J6dpFiVkI" crossorigin="anonymous"></script>';
+                    break;
+            }
 
-		// Supply list of tags to ignore.
-		if (!empty($options['ignoredTags'])) {
-			$htmlOutput .= 'ignoredTags:' . json_encode($options['ignoredTags']) . ',';
-		}
+            // Supply HTML DOM element.
+            $htmlOutput .= '<script>document.addEventListener("DOMContentLoaded",function(){renderMathInElement(' . $options['element'] . ',';
 
-		// Supply list of classes to ignore.
-		if (!empty($options['ignoredClasses'])) {
-			$htmlOutput .= 'ignoredClasses:' . json_encode($options['ignoredClasses']) . ',';
-		}
+            // Supply delimiters.
+            $htmlOutput .= '{delimiters:' . json_encode($options['delimiters']) . ',';
 
-		// Supply error callback function.
-		if (!is_null($options['errorCallback'])) {
-			$htmlOutput .= 'errorCallback:' . $options['errorCallback'];
-		}
+            // Supply list of tags to ignore.
+            if (!empty($options['ignoredTags'])) {
+                $htmlOutput .= 'ignoredTags:' . json_encode($options['ignoredTags']) . ',';
+            }
 
-		// Supply callback function to process math expressions before rendering.
-		if (!is_null($options['preProcess'])) {
-			$htmlOutput .= 'preProcess:' . $options['preProcess'];
-		}
+            // Supply list of classes to ignore.
+            if (!empty($options['ignoredClasses'])) {
+                $htmlOutput .= 'ignoredClasses:' . json_encode($options['ignoredClasses']) . ',';
+            }
 
-		$htmlOutput .= '});});</script><!-- antstei/automath-chunk end -->';
+            // Supply error callback function.
+            if (!is_null($options['errorCallback'])) {
+                $htmlOutput .= 'errorCallback:' . $options['errorCallback'];
+            }
 
-		return $htmlOutput;
-	}
+            // Supply callback function to process math expressions before rendering.
+            if (!is_null($options['preProcess'])) {
+                $htmlOutput .= 'preProcess:' . $options['preProcess'];
+            }
 
-	/**
-	 * 	Helper function to get human readable error description of the last JSON error.
-	 *
-	 * 	@return string JSON error description
-	 */
+            /* Create end of KaTeX bootstrap code string. */
+            $htmlOutput .= '});});</script><!-- antstei/automath-chunk end -->';
+            Automad\Debug::log($options, 'Created KaTeX bootstrap code string successfully using the following options');
+        }
 
-	private static function getJSONErrorDescription() {
-		switch(json_last_error()) {
-        case JSON_ERROR_DEPTH:
-            return 'stack depth error';
-        break;
-        case JSON_ERROR_STATE_MISMATCH:
-            return 'state mismatch error';
-        break;
-        case JSON_ERROR_CTRL_CHAR:
-            return 'CTRL character error';
-        break;
-        case JSON_ERROR_SYNTAX:
-            return 'syntax error';
-        break;
-        case JSON_ERROR_UTF8:
-            return 'UTF8 encoding error';
-        break;
-        default:
-            return 'unknown error';
-        break;
+        return $htmlOutput;
     }
-	}
+
+    /**
+     * 	Helper function to get human readable error description of the last JSON error.
+     *
+     * 	@return string JSON error description
+     */
+
+    private static function getJSONErrorDescription() {
+        switch(json_last_error()) {
+            case JSON_ERROR_DEPTH:
+                return 'stack depth error';
+                break;
+            case JSON_ERROR_STATE_MISMATCH:
+                return 'state mismatch error';
+                break;
+            case JSON_ERROR_CTRL_CHAR:
+                return 'CTRL character error';
+                break;
+            case JSON_ERROR_SYNTAX:
+                return 'syntax error';
+                break;
+            case JSON_ERROR_UTF8:
+                return 'UTF8 encoding error';
+                break;
+            default:
+                return 'unknown error (' . json_last_error() . ')';
+                break;
+        }
+    }
 }
--- README.md
+++ README.md
@@ -10,7 +10,7 @@ This extension can be enabled on pages by calling
 
     <@ Antstei/Automath { options } @>
 
-inside the head section (`<head> ... </head>`) of the respective template. By putting inside [automad's blocks](https://automad.org/developer-guide/building-themes/template-language/variables#blocks) by KaTeX [supported TeX functions](https://katex.org/docs/supported.html) between two pairs of dollar signs (`$` ... `$`), such as
+inside the head section (`<head> ... </head>`) of the respective template. By putting inside [Automad's blocks](https://automad.org/developer-guide/building-themes/template-language/variables#blocks) by KaTeX [supported TeX functions](https://katex.org/docs/supported.html) between two pairs of dollar signs (`$` ... `$`), such as
 
     $\bigl(\frac{a}{p}\bigr)$
 
@@ -22,12 +22,14 @@ mathematical notations are rendered on page load by the user's web browser:
 
 The following options are available:
 
-| Name | Default value |
-|------|---------------|
-| `source` | `'cdn'` |
-| `defer` | `false` |
-| [`element`](https://katex.org/docs/autorender.html#api) | `'document.body'` |
-| [`delimiters`](https://katex.org/docs/autorender.html#api) | `'[{"left": "$", "right": "$", "display": false}]'` |
-| [`ignoredTags`](https://katex.org/docs/autorender.html#api) | *By default not set*, hence KaTeX default value `["script", "noscript", "style", "textarea", "pre", "code"]` is used. |
-| [`ignoredClasses`](https://katex.org/docs/autorender.html#api) | *By default not set*. |
-| [`errorCallback`](https://katex.org/docs/autorender.html#api) | *By default not set*. |
+| Name | Default value | Description |
+|------|---------------| ----------- |
+| `source` | `'cdn'` | Specifies the include method of KaTeX's JavaScript files. Currently `'cdn'` is the only available method. |
+| `defer` | `false` | Specifies whether KaTeX's JavaScript code doesn't need to execute until execute the whole page has loaded. A `true` value can speed up page rendering. |
+| `includedPageTags` | `'[]'` | Specifies a list of Automad's page tags to include KaTeX's bootstrap code on those tagged pages. An empty array (`[]`) indicates that the KaTeX's bootstrap code is included on all pages regardless of the current page tags. |
+| `includedQueryFields` | `'["filter", "search"]'` | Specifies a list of URL query string fields, such as `filter` or `search`, to include KaTeX's bootstrap code when those fields are part of the current page's URL. An empty array (`[]`) indicates that the KaTeX's bootstrap code is included on all pages regardless of the current page's URL. |
+| [`element`](https://katex.org/docs/autorender.html#api) | `'document.body'` | |
+| [`delimiters`](https://katex.org/docs/autorender.html#api) | `'[{"left": "$", "right": "$", "display": false}]'` | |
+| [`ignoredTags`](https://katex.org/docs/autorender.html#api) | *By default, not set*, hence KaTeX's default value `["script", "noscript", "style", "textarea", "pre", "code"]` is used. | |
+| [`ignoredClasses`](https://katex.org/docs/autorender.html#api) | *By default, not set*. | |
+| [`errorCallback`](https://katex.org/docs/autorender.html#api) | *By default, not set*. | |
